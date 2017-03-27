6e364b82a08d1a7f5419846adb4ef7a75fae4e92
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2017-03-27 21:49:01 +0100

    Chapter 1

---
+++ tests.py
@@ -0,0 +1,11 @@
+from test_case import TestCase
+from was_run import WasRun
+
+class TestCaseTest(TestCase):
+    def test_running(self):
+        test = WasRun("test_method")
+        assert(not test.was_run)
+        test.run()
+        assert(test.was_run)
+
+TestCaseTest("test_running").run()
---
+++ was_run.py
@@ -0,0 +1,10 @@
+from test_case import TestCase
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.was_run = None
+        TestCase.__init__(self, name)
+
+    def test_method(self):
+        self.was_run = 1
+
